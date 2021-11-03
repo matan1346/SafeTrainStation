4fc80f524a72d4ab3fe8e3c9cde06cf5fefcc40b
Matan Omesi <matanfxp@hotmail.co.il>
2021-11-03 15:20:38 +0200

    Update stations.service.ts

--- angular-web/src/app/core/services/stations.service.ts
+++ angular-web/src/app/core/services/stations.service.ts
@@ -15,57 +15,7 @@ export class StationsService {
   private firstTimeLoaded = false;
   constructor(private httpClient: HttpClient) {
 
-    //this.loadStations();
-
-    // let stations: Station[] = [{id: 1, city: 'Tel Aviv', name: 'HaHagana'},
-    // {id: 2, city: 'Jerusalem', name: 'Itzhak Navon'},
-    //  {id: 3, city: 'Tel Aviv', name: 'Ha-Shalom'},
-    //  {id: 4, city: 'Be`er Sheva', name: 'Tzafon'},
-    //  {id: 5, city: 'Tel Aviv', name: 'Hanna'},
-    //  {id: 6, city: 'Tel Aviv', name: 'University'},
-    //  {id: 7, city: 'Rishon Letzion', name: 'Moshe Dayan'},
-    //  {id: 8, city: 'Rishon Letzion', name: 'Ha-rishonim'},
-    //  {id: 9, city: 'Jerusalem', name: 'Malcha'},
-    //  {id: 10, city: 'Jerusalem', name: 'Animals Garden'},
-    // {id: 11, city: 'Ben Gurion', name: 'Bun Gurion Airport'},
-    //  {id: 12, city: 'Be`er Sheva', name: 'Merkaz'}];
-
-
-
-    // let cameras: Camera[] = [];
-    // for(let i=0;i < 100;i++){
-    //   cameras.push({id: i + 1, name: `Camera ${i + 1}`, active: true});
-    // }
-
-    // let amount = cameras.length;
-    // let j = 0;
-    // for(let i=0; i < stations.length;++i){
-    //   let current = this.randomIntFromInterval(0, 10);
-    //   //j += current;
-    //   stations[i].cameras = [];
-
-    //   for(let k = 0;k < current && (j + k) < cameras.length;++k)
-    //     stations[i].cameras.push(cameras[j + k]);
-    //   j += current;
-    //   amount -= current;
-
-    // }
-
-    // let camera1: Camera = {id: 1, name: 'Camera 1', active: true};
-    // let camera2: Camera = {id: 2, name: 'Camera 2', active: true};
-    // let camera3: Camera = {id: 3, name: 'Camera 3', active: true};
-    // let camera4: Camera = {id: 4, name: 'Camera 4', active: true};
-    // let camera5: Camera = {id: 5, name: 'Camera 5', active: true};
-
-
-    //this.stationsData$.next(stations);
-
-
-
-
-
-
-
+    this.loadStations();
    }
 
    async initStations(){
